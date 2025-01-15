3e531c6918e58cb67d47390aa59d7f190e050d65
jian.yin <jian.yin@shopee.com>
2025-01-15 03:53:31 +0000

    feat: add direct path to run a service without docker command.

--- fastchat/llm_judge/judge_workflow.sh
+++ fastchat/llm_judge/judge_workflow.sh
@@ -9,6 +9,7 @@ Usage: All options are needed!
     -m: model_name
     -b: backend (e.g., lmdeploy/vllm/tensorrt-llm)
     -s: service port (default: 8080)
+    -n: parallel num (default: 50)
 EOF
 }
 
@@ -16,6 +17,7 @@ EOF
 service_port=8080
 backend="lmdeploy"
 tp=1
+parallel=50
 
 # Parse command-line arguments
 while getopts "p:d:t:m:b:s:h" opt; do
@@ -26,6 +28,7 @@ while getopts "p:d:t:m:b:s:h" opt; do
         m) model_name="$OPTARG" ;;
         b) backend="$OPTARG" ;;
         s) service_port="$OPTARG" ;;
+        n) parallel="$OPTARG" ;;
         h) usage; exit 0 ;;
         *) echo "Invalid option: -$OPTARG"; usage; exit 1 ;;
     esac
@@ -43,41 +46,59 @@ LMDEPLOY_IMAGE_TAG="harbor.shopeemobile.com/aip/shopee-mlp-aip-llm-generater-lmd
 CONTAINER_NAME="LLMjudge-test"
 service_name="0.0.0.0"
 
+declare -g lmdeploy_pid=""
 # Function to start the model server
 function open_model_server() {
     echo "Starting model server..."
     if [[ $backend == "lmdeploy" ]]; then
-        docker run -d --gpus all --env CUDA_VISIBLE_DEVICES=$device_id \
-        --privileged --shm-size=10g --ipc=host \
-        -v ${model_path}:/workspace/models/${model_path} \
-        -p ${service_port}:${service_port} \
-        --name=${CONTAINER_NAME} ${LMDEPLOY_IMAGE_TAG} \
-        lmdeploy serve api_server \
-            /workspace/models/${model_path} \
-            --server-name ${service_name} \
-            --server-port ${service_port} \
-            --tp ${tp} \
-            --max-batch-size 512 \
-            --cache-max-entry-count 0.9 \
-            --session-len 8192
+        if command -v lmdeploy >/dev/null 2>&1; then
+            lmdeploy serve api_server \
+                ${model_path} \
+                --server-name ${service_name} \
+                --server-port ${service_port} \
+                --tp ${tp} \
+                --max-batch-size 512 \
+                --cache-max-entry-count 0.9 \
+                --session-len 8192 &
+            lmdeploy_pid=$!
+        else
+            docker run -d --gpus all --env CUDA_VISIBLE_DEVICES=$device_id \
+                --privileged --shm-size=10g --ipc=host \
+                -v ${model_path}:/workspace/models/${model_path} \
+                -p ${service_port}:${service_port} \
+                --name=${CONTAINER_NAME} ${LMDEPLOY_IMAGE_TAG} \
+                lmdeploy serve api_server \
+                    /workspace/models/${model_path} \
+                    --server-name ${service_name} \
+                    --server-port ${service_port} \
+                    --tp ${tp} \
+                    --max-batch-size 512 \
+                    --cache-max-entry-count 0.9 \
+                    --session-len 8192
+            sleep 5m
+        fi
     else
         echo "Invalid backend specified: $backend"
         exit 1
     fi
 
     echo "Waiting for server to start..."
-    sleep 5m
 }
 
 # Function to stop the model server
 function close_model_server() {
     echo "Stopping model server..."
-    container_id=$(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $1 }')
-    if [[ -n "$container_id" ]]; then
-        docker stop ${container_id}
-        docker rm ${container_id}
+    if command -v lmdeploy >/dev/null 2>&1; then
+	    # pkill -f "lmdeploy serve api_server"
+	    kill -9 "$lmdeploy_pid"
     else
-        echo "No container found with name ${CONTAINER_NAME}"
+    	container_id=$(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $1 }')
+    	if [[ -n "$container_id" ]]; then
+        	docker stop ${container_id}
+        	docker rm ${container_id}
+    	else
+        	echo "No container found with name ${CONTAINER_NAME}"
+    	fi
     fi
 }
 
@@ -85,15 +106,17 @@ function close_model_server() {
 open_model_server
 
 # Run the pairwise result generation script
+cd ../..
 pip install -e ".[model_worker,llm_judge]"
 pip install --upgrade openai
+cd fastchat/llm_judge
 
 export OPENAI_API_KEY=sk-2c27e4a764b24dd79d83e6c6e65362fa
 export OPENAI_API_BASE=https://api.deepseek.com
 
 python3 gen_pairwise_result.py \
     --model-list $model_name \
-    --parallel 50 \
+    --parallel $parallel \
     --openai-api-base http://$service_name:$service_port \
     --local-api \
     --mode pairwise-all
